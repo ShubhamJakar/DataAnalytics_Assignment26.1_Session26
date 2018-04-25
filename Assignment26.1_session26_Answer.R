4ff79de77a36ff6dfafb742d49f387f1090c0aa5
Shubham Jakar <34440552+ShubhamJakar@users.noreply.github.com>
2018-04-25 15:51:22 +0530

    Add files via upload

---
+++ Assignment26.1_session26_Answer.R
@@ -0,0 +1,19 @@
+#Assignment26.1_Session26
+
+#Problem
+#Requirements
+#For each question, you are required to provide the following:
+#- The SQL query you used
+#- The answers
+#- Any assumptions you made
+#Problem Statement
+#1 What are the names of all the countries in the databases which start with the letter "B"
+# (sorted by their names)?
+#2. Return the first names (sorted) of all the actors with the last name "berry".
+#3 Find all the films whose length is more than 184 (inclusive). Order the results by the length
+#(and for films with the same length order them by their name). Return their title and the length.
+
+
+#Answers
+#topics are not covered yet,as soon as they are teached i will do this!!
+
